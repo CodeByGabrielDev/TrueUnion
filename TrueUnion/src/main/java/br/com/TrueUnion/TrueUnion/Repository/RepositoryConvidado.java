36bada2796883dce7e684289767ea7bb6d073417
Gabriel Lima | Dev <ogabriellima1999@gmail.com>
2025-10-22 09:38:21 -0300

    Delete TrueUnion directory

--- TrueUnion/src/main/java/br/com/TrueUnion/TrueUnion/Repository/RepositoryConvidado.java
+++
@@ -1,9 +0,0 @@
-package br.com.TrueUnion.TrueUnion.Repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import br.com.TrueUnion.TrueUnion.Entities.Convidado;
-
-public interface RepositoryConvidado extends CrudRepository<Convidado, Integer> {
-
-}
--- TrueUnion/src/main/java/br/com/TrueUnion/TrueUnion/Repository/RepositoryPerfilUsuario.java
+++
@@ -1,9 +0,0 @@
-package br.com.TrueUnion.TrueUnion.Repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import br.com.TrueUnion.TrueUnion.Entities.PerfilUsuario;
-
-public interface RepositoryPerfilUsuario extends CrudRepository<PerfilUsuario, Integer> {
-
-}
